dce9022cfb39f0f2c8ff1fac882b15311baa99f8
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-14 17:38:57 +0530

    redux further

--- redux-demo.js
+++ redux-demo.js
@@ -1,8 +1,16 @@
 const redux = require("redux");
 const counterReducer = (state = {counter: 0}, action) => {
-    return {
-        counter: state.counter + 1
+    if(action.type === "increment") {
+        return {
+            counter: state.counter + 1
+        }
     }
+    if (action.type === "decrement") {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state;
 };
 const store = redux.createStore(counterReducer);
 
@@ -13,3 +21,9 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber);
 store.dispatch({type: "increment"});
+store.dispatch({type: "increment"});
+store.dispatch({type: "increment"});
+store.dispatch({type: "increment"});
+store.dispatch({type: "increment"});
+store.dispatch({type: "decrement"});
+store.dispatch({type: "decrement"});
